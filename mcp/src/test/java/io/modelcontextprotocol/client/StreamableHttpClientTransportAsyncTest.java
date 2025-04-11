3491098019d2d1cd3cbbb73fed10893f3a8fa5e0
a.darafeyeu <Aliaksei_Darafeyeu@epam.com>
2025-04-11 14:48:28 +0200

    feat(client): adds StreamableHttpClientTransport

---
+++ mcp/src/test/java/io/modelcontextprotocol/client/StreamableHttpClientTransportAsyncTest.java
@@ -0,0 +1,44 @@
+package io.modelcontextprotocol.client;
+
+import org.junit.jupiter.api.Timeout;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+import io.modelcontextprotocol.client.transport.StreamableHttpClientTransport;
+import io.modelcontextprotocol.spec.McpClientTransport;
+
+/**
+ * Tests for the {@link McpAsyncClient} with {@link StreamableHttpClientTransport}.
+ *
+ * @author Aliaksei Darafeyeu
+ */
+@Timeout(15)
+public class StreamableHttpClientTransportAsyncTest extends AbstractMcpAsyncClientTests {
+
+	String host = "http://localhost:3003";
+
+	// Uses the https://github.com/tzolov/mcp-everything-server-docker-image
+	@SuppressWarnings("resource")
+	GenericContainer<?> container = new GenericContainer<>("docker.io/tzolov/mcp-everything-server:v1")
+		.withLogConsumer(outputFrame -> System.out.println(outputFrame.getUtf8String()))
+		.withExposedPorts(3001)
+		.waitingFor(Wait.forHttp("/").forStatusCode(404));
+
+	@Override
+	protected McpClientTransport createMcpTransport() {
+		return StreamableHttpClientTransport.builder(host).build();
+	}
+
+	@Override
+	protected void onStart() {
+		container.start();
+		int port = container.getMappedPort(3001);
+		host = "http://" + container.getHost() + ":" + port;
+	}
+
+	@Override
+	protected void onClose() {
+		container.stop();
+	}
+
+}
